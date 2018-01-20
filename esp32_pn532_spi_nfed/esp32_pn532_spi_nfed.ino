ecacb6b1f18273bab57fbcadc64177c55a81382b
samatthias <accounts@m-mosimann.com>
2018-01-20 21:46:57 +0100

    Add files via upload

---
+++ esp32_pn532_spi_nfed/esp32_pn532_spi_nfed.ino
@@ -0,0 +1,25 @@
+
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 5);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+
+void setup(void) {
+
+    Serial.begin(115200);
+    Serial.println("NDEF Reader");
+    nfc.begin();
+}
+
+void loop(void) {
+    Serial.println("\nScan a NFC tag\n");
+    if (nfc.tagPresent())
+    {
+        NfcTag tag = nfc.read();
+        tag.print();
+    }
+    delay(2000);
+}
